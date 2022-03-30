c7a453e715e5c1b45083b4f9bf1d9dbb8dccbbc9
= <=>
2022-03-30 19:15:45 -0400

    ran jest coverage test

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,6 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
 import App from './App';
-import { JustifyLeft } from 'react-bootstrap-icons';
 
 //get fetch before mock
 const unmockedFetch = global.fetch;
